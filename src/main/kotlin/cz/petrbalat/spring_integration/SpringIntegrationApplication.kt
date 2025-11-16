2ce476a3562edfb273b5746c0f61935b68d052af
Petr Balat <petr.balat@gmail.com>
2025-11-16 12:09:15 +0100

    spring boot with web and spring integration

---
+++ src/main/kotlin/cz/petrbalat/spring_integration/SpringIntegrationApplication.kt
@@ -0,0 +1,11 @@
+package cz.petrbalat.spring_integration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringIntegrationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringIntegrationApplication>(*args)
+}
